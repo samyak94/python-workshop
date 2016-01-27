fdd5d7c8030dcfb4ff599af0fb6df68242b31f0b
Samyak Anand <samyak.anand@gmail.com>
2016-01-27 15:23:12 +0530

    string opt again

--- python.py
+++ python.py
@@ -16,12 +16,7 @@ Was as fresh as the bright blue sky
 - Sweet Child O' Mine(Guns and Roses)"""
 print longString
 
-longString = '''She's got a smile that it seems to me
-Reminds me of childhood memories
-Where everything
-Was as fresh as the bright blue sky
-- Sweet Child O' Mine(Guns and Roses)'''
-print longString
+
 
 myLongstring = '''She's got a smile that it seems to me \
 Reminds me of childhood memories \
