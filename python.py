e62823f05c906ce7a5b5a85d64359880078dad00
Samyak Anand <samyak.anand@gmail.com>
2016-01-27 15:20:17 +0530

    string operation in details

--- python.py
+++ python.py
@@ -8,3 +8,24 @@ print(x,z,y)
 
 mavya= "zen of python"
 print mavya
+
+longString = """She's got a smile that it seems to me
+Reminds me of childhood memories
+Where everything
+Was as fresh as the bright blue sky
+- Sweet Child O' Mine(Guns and Roses)"""
+print longString
+
+longString = '''She's got a smile that it seems to me
+Reminds me of childhood memories
+Where everything
+Was as fresh as the bright blue sky
+- Sweet Child O' Mine(Guns and Roses)'''
+print longString
+
+myLongstring = '''She's got a smile that it seems to me \
+Reminds me of childhood memories \
+Where everything \
+Was as fresh as the bright blue sky \
+- Sweet Child O' Mine(Guns and Roses)'''
+print myLongstring
